cbd2cc2e8b8aacda2228b9eb3be0fd3b3066669b
Cyrus Behroozi <cyrus.behr@gmail.com>
2016-12-14 10:46:54 -0800

    first way to present alert

--- LyndaTutorials/ViewController.swift
+++ LyndaTutorials/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var showAlertButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func showAlertButtonPressed(_ sender: Any) {
+    //Create alert controller
+    let alert: UIAlertController = UIAlertController(title:"Title", message: "Message (note these values can be set to nil)", preferredStyle: .alert)
+    
+    let action1: UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) { (_:UIAlertAction) in
+    print("Put cancel actions here")
+    }
+    
+    alert.addAction((action1)
+    self.present(alert, animated: true, completion: { 
+      print("present complete")
+    })
 
 }
 
