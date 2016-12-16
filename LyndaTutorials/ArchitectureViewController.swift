93184dd22728e1e476b557aed03b5090b4f2f8a2
Cyrus Behroozi <cyrus.behr@gmail.com>
2016-12-16 10:34:28 -0800

    added delegate viewController methods for controlling view activities

---
+++ LyndaTutorials/ArchitectureViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ArchitectureViewController.swift
+//  LyndaTutorials
+//
+//  Created by Cyrus Behroozi on 2016-12-16.
+//  Copyright © 2016 Cyrus Behroozi. All rights reserved.
+//
+
+import UIKit
+
+class ArchitectureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    <#code#>
+  }
+  override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    <#code#>
+  }
+  override func viewWillDisappear(_ animated: Bool) {
+    <#code#>
+  }
+  
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
